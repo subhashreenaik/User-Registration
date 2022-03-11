cb20928db784c27a5263682bd7e96ba748a50dd6
Subhashree naik <subhashree.naik@gmail.com>
2022-03-11 22:10:52 +0530

    Minimum 8 characher required

--- src/com/assignment/day19/UserRegistration.java
+++ src/com/assignment/day19/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 	
 	/* Method to check validation of First and last name
 	 *  where regular expression of first and last name is defined */
-	public static boolean validForName(String s) 
+public static boolean validForName(String s) 
 	    {
 		
 		Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
@@ -20,7 +20,7 @@ public class UserRegistration {
 	    }
 	/* Method to check validation of EmailId
 	 *  where regular expression of emailId is defined */
-    public static boolean validEmailId(String s) {
+public static boolean validEmailId(String s) {
 		
 		Pattern p = Pattern.compile("[a-z]{2,}(((.?)[+-]*(_?)*[a-z0-9]*){2,})*@[a-z]{1,}.[a-z]{1,}(.[a-z]*{1,})*");
 		Matcher m = p.matcher(s);
@@ -38,6 +38,15 @@ public class UserRegistration {
 		return b;
 		
 	    }
+ public static boolean validPassword(String s) {
+		Pattern p = Pattern.compile("([A-Za-z0-9]){8,}");
+		Matcher m = p.matcher(s);
+		boolean b = m.matches(); 
+		
+		return b;
+		
+		}
+
 
 	public static void main(String[] args) {
 		
@@ -52,6 +61,8 @@ public class UserRegistration {
 		String emailid = s.nextLine();
 		System.out.println("Enter your mobile Number  :");
 		String mobileno = s.nextLine();
+		System.out.println("Enter your Password  :");
+		String password = s.nextLine();
 		
 		if(validForName(firstname) != true) 
 		{
@@ -69,10 +80,14 @@ public class UserRegistration {
 		{
 		System.out.println("Please give a valid mobile Number");	
 		}
+		if(validPassword(password) != true) 
+		{
+		System.out.println("Please give a valid Password");	
+		}
 		else 
 		{
 		System.out.println("String is valid");
 		}
 	}
-
+	 
 }
