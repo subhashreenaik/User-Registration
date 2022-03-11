4173a7ec743b804ea84c595269a910b1dcdaf2f7
Subhashree naik <subhashree.naik@gmail.com>
2022-03-11 20:11:53 +0530

    Email Id Validation

--- src/com/assignment/day19/UserRegistration.java
+++ src/com/assignment/day19/UserRegistration.java
@@ -8,7 +8,8 @@ public class UserRegistration {
 	
 	/* Method to check validation of First and last name
 	 *  where regular expression of first and last name is defined */
-	public static boolean validForName(String s) {
+	public static boolean validForName(String s) 
+	    {
 		
 		Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
 		Matcher m = p.matcher(s);
@@ -16,7 +17,18 @@ public class UserRegistration {
 		
 		return b;
 		
-	}
+	    }
+	/* Method to check validation of EmailId
+	 *  where regular expression of emailId is defined */
+    public static boolean validEmailId(String s) {
+		
+		Pattern p = Pattern.compile("[a-z]{2,}(((.?)(_?)*[a-z0-9]*){2,})*@[a-z]{1,}.[a-z]{1,}(.[a-z]*{1,})*");
+		Matcher m = p.matcher(s);
+		boolean b = m.matches(); 
+		
+		return b;
+		
+	    }
 
 	public static void main(String[] args) {
 		
@@ -27,6 +39,8 @@ public class UserRegistration {
 		String firstname = s.nextLine();
 		System.out.println("Enter your last name  :");
 		String lastname = s.nextLine();
+		System.out.println("Enter your emailID  :");
+		String emailid = s.nextLine();
 		
 		
 		if(validForName(firstname) != true) 
@@ -37,9 +51,13 @@ public class UserRegistration {
 		{
 		System.out.println("Last name should start with Capital letter and has minimum 3 character");	
 		}
+		if(validEmailId(emailid) != true) 
+		{
+		System.out.println("Please give a valid emailID");	
+		}
 		else 
 		{
-		System.out.println("Name is valid");
+		System.out.println("String is valid");
 		}
 	}
 
